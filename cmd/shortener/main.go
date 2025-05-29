0484a872c5f079c14ea2a45ae1dd09b6bdaf184f
Константин Зубченко <k.zubchenko@bi.zone>
2025-05-30 01:29:14 +0300

    Default handlers and routing for them

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"github.com/JohnnyConstantin/urlshort/internal/app"
+	"net/http"
+)
+
+func main() {
+	var s app.Server
+	server := s.NewServer()
+
+	//Чтобы удобнее было работать
+	handler := server.Handler
+	router := server.Router
+
+	//Накидываем хендлеры на роуты
+	router.AddRoute("/", http.MethodPost, handler.PostHandler)
+	router.AddRoute("/id/", http.MethodGet, handler.GetHandler)
+
+	//Регаем handlers через стандартную либу
+	http.Handle("/", router)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+
+}
--- internal/app/handler.go
+++ internal/app/handler.go
@@ -1,2 +1,31 @@
 package app
 
+import (
+	"net/http"
+)
+
+type Handler struct {
+	router *Router
+}
+
+func NewHandler() *Handler {
+	h := &Handler{
+		router: NewRouter(),
+	}
+
+	return h
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.router.ServeHTTP(w, r)
+}
+
+// GetHandler handles GET reqs
+func (h *Handler) GetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Это GET хендлер"))
+}
+
+// PostHandler handles POsT reqs
+func (h *Handler) PostHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Это POST хендлер"))
+}
