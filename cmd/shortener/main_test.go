404f42d21e07286c362a40915589cc288d68cf8e
Константин Зубченко <k.zubchenko@bi.zone>
2025-06-05 23:58:47 +0300

    Tests for all functional packages

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"github.com/JohnnyConstantin/urlshort/internal/app"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// TestServerCreation проверяет корректность создания сервера
+func TestServerCreation(t *testing.T) {
+	var s app.Server
+	server := s.NewServer()
+	router := server.Router
+	handler := server.Handler
+
+	require.NotNilf(t, server, "Expected server instance, got nil")
+
+	require.NotNilf(t, router, "Expected router to be initialized, got nil")
+
+	require.NotNilf(t, handler, "Expected handler to be initialized, got nil")
+}
