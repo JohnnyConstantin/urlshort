4945acc0ec3bac1513b6ff5909f14e42ba8c5681
Константин Зубченко <k.zubchenko@bi.zone>
2025-06-05 22:34:22 +0300

    Fix var for ENV

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,6 @@
 package config
 
 var (
-	appName     = "shortener"
-	PATH_TO_ENV = "../../.env"
+	AppName   = "shortener"
+	PathToENV = "../../.env"
 )
--- internal/store/sql.go
+++ internal/store/sql.go
@@ -14,7 +14,7 @@ import (
 func GetDB() (*sql.DB, error) {
 
 	//Вгружаем переменные окружения, в т.ч. креды для бд
-	err := godotenv.Load(config.PATH_TO_ENV)
+	err := godotenv.Load(config.PathToENV)
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
